1e650addac5766fc812a0cc1ac01d8eb74509ecb
atharvapat47 <nicpat47@gmail.com>
2026-03-16 11:46:22 +1100

    register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -1,11 +1,36 @@
-if ("serviceWorker" in navigator) {
- window.addEventListener("load", () => {
-   navigator.serviceWorker.register("/pwa-test/sw.js")
-     .then(reg => {
-       console.log("Service Worker Registered");
-     })
-     .catch(err => {
-       console.log("Service Worker Failed", err);
-     });
- });
-}
+const CACHE_NAME = "tap-the-blob-v1";
+const FILES_TO_CACHE = [
+ "/pwa-test/",
+ "/pwa-test/index.html",
+ "/pwa-test/style.css",
+ "/pwa-test/app.js",
+ "/pwa-test/manifest.json",
+ "/pwa-test/register-sw.js",
+ "/pwa-test/icons/icon-192.png",
+ "/pwa-test/icons/icon-512.png"
+];
+self.addEventListener("install", (event) => {
+ event.waitUntil(
+   caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+ );
+ self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+ event.waitUntil(
+   caches.keys().then((keys) =>
+     Promise.all(
+       keys
+         .filter((key) => key !== CACHE_NAME)
+         .map((key) => caches.delete(key))
+     )
+   )
+ );
+ self.clients.claim();
+});
+self.addEventListener("fetch", (event) => {
+ event.respondWith(
+   caches.match(event.request).then((cachedResponse) => {
+     return cachedResponse || fetch(event.request);
+   })
+ );
+});
